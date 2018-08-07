7e9a44973eeff777eeb6474025ff5fdb16915b3a
Chris Wilcoxson <chris@slushman.com>
2018-08-06 23:15:58 -0500

    Initial commit.

---
+++ functions.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Core theme file.
+ *
+ * @package  Slushless
+ * @author   slushman
+ * @license  GPL-2.0+
+ * @link     https://developer.wordpress.org/themes/basics/theme-functions/
+ */
+
+add_action( 'after_setup_theme', 'slushless_theme_setup' );
+add_action( 'after_setup_theme', 'slushless_text_domain' );
+add_action( 'after_setup_theme', 'slushless_register_menus' );
+
+
+/**
+ * Registers Menus
+ *
+ * @hooked 		after_setup_theme
+ */
+function slushless_register_menus() {
+
+	register_nav_menus( array(
+		'main' 		=> esc_html__( 'Main', 'slushless' ),
+		'social' 	=> esc_html__( 'Social', 'slushless' ),
+		'footer' 	=> esc_html__( 'Footer', 'slushless' )
+	) );
+
+} // slushless_register_menus()
+
+/*
+ * Make theme available for translation.
+ * Translations can be filed in the /languages/ directory.
+ *
+ * @hooked 		after_setup_theme
+ */
+function slushless_text_domain() {
+
+	load_theme_textdomain( 'slushless', get_template_directory() . '/languages' );
+
+} // slushless_text_domain()
+
+/**
+ * Sets up basic items needed for the theme to work.
+ */
+function slushless_theme_setup() {
+
+	add_theme_support( 'post-thumbnails' );
+
+} // slushless_theme_setup()
---
+++ index.php
@@ -0,0 +1,6 @@
+<meta content="0; URL='https://www.slushman.com''" http-equiv"refresh">
+
+<!-- just in case the meta tag is not read properly, here is plan B: a JS redirect -->
+<script type="text/javascript">
+	window.location = 'https://www.slushman.com';
+</script>
