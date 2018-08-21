93df0729eb2b58cd789fd686d470d2984242a831
Chris Wilcoxson <chris@slushman.com>
2018-08-21 09:52:57 -0500

    Remove br tags from galleries.

--- functions.php
+++ functions.php
@@ -2,23 +2,26 @@
 /**
  * Core theme file.
  *
- * @package  Slushless
+ * @package  Headless
  * @author   slushman
  * @license  GPL-2.0+
  * @link     https://developer.wordpress.org/themes/basics/theme-functions/
  */
-add_action( 'after_setup_theme', 'slushless_theme_setup' );
-add_action( 'after_setup_theme', 'slushless_text_domain' );
-add_action( 'after_setup_theme', 'slushless_register_menus' );
+add_action( 'after_setup_theme', 'headless_theme_setup' );
+add_action( 'after_setup_theme', 'headless_text_domain' );
+add_action( 'after_setup_theme', 'headless_register_menus' );
+
 add_filter( 'use_default_gallery_style', '__return_false' );
+add_filter( 'the_content', 'headless_remove_brs_from_galleries', 11, 2 );
 
 
 /**
  * Registers Menus
  *
  * @hooked 		after_setup_theme
+ * @since 		1.0.0
  */
-function slushless_register_menus() {
+function headless_register_menus() {
 
 	register_nav_menus( array(
 		'main' 		=> esc_html__( 'Main', 'slushless' ),
@@ -26,25 +29,43 @@ function slushless_register_menus() {
 		'footer' 	=> esc_html__( 'Footer', 'slushless' )
 	) );
 
-} // slushless_register_menus()
+} // headless_register_menus()
+
+/**
+ * Removes the random br tags from WordPress galleries.
+ * 
+ * @hooked 		the_content
+ * @since 		1.0.2
+ * @param 		mixed 		$output 		The post content.
+ * @param 		mixed 						The modified post content.
+ */
+function headless_remove_brs_from_galleries( $output ) {
+
+	return preg_replace( '/\<br[^\>]*\>/', '', $output );
+
+} // headless_remove_brs_from_galleries()
 
 /*
  * Make theme available for translation.
  * Translations can be filed in the /languages/ directory.
  *
  * @hooked 		after_setup_theme
+ * @since 		1.0.0
  */
-function slushless_text_domain() {
+function headless_text_domain() {
 
-	load_theme_textdomain( 'slushless', get_template_directory() . '/languages' );
+	load_theme_textdomain( 'headless', get_template_directory() . '/languages' );
 
-} // slushless_text_domain()
+} // headless_text_domain()
 
 /**
  * Sets up basic items needed for the theme to work.
+ * 
+ * @hooked 		after_setup_theme
+ * @since 		1.0.0
  */
-function slushless_theme_setup() {
+function headless_theme_setup() {
 
 	add_theme_support( 'post-thumbnails' );
 
-} // slushless_theme_setup()
+} // headless_theme_setup()
